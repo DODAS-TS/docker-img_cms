72aa7d64df20481e52598b118d4983f545bcc93f
spigad <spiga@pg.infn.it>
2018-11-09 18:58:24 +0100

    add partitionable slot dynamic configuration

--- dodas_bin/dodasexe_pre.sh
+++ dodas_bin/dodasexe_pre.sh
@@ -45,6 +45,9 @@ if [ $? -eq 0 ]; then
     sed -i -e "s/$str2/GLIDEIN_CMSSite = \"$CMS_LOCAL_SITE\"/g" /etc/condor/config.d/99_DODAS_local
     str3=$(grep "GLIDEIN_Gatekeeper =" /etc/condor/config.d/99_DODAS_local)
     sed -i -e "s/$str3/GLIDEIN_Gatekeeper = \"$GATKEEPER\"/g" /etc/condor/config.d/99_DODAS_local
+    str4=$(grep "NUM_CPUS" /etc/condor/config.d/03_DODAS_Partitionable_Slots)
+    sed -i -e "s/$str4/NUM_CPUS = ${MARATHON_APP_RESOURCE_CPUS%.*}/g" /etc/condor/config.d/03_DODAS_Partitionable_Slots
+
 
     COLLECTOR_PORT=`shuf -i 9621-9720 -n 1`
     sed -i -e "s/COLLECTOR_PORT/${COLLECTOR_PORT}/g" /etc/condor/config.d/99_DODAS_tweaks
